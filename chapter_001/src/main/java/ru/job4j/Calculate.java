1685062f742e36d166528da131aba737db434584
Andrey Filev <aands2002@mail.ru>
2018-01-13 13:37:24 +0300

    1.2. Подключить сборщик Maven[#4086]

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class Calculate {
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	} 
+};
+
+/*
+  public String echo(String value){
+	return String.format("%s %s %s", value, value, value);
+  }
+
+  public static void main(String[] args){
+	Calculate calc = new Calculate();
+	System.out.println(calc.echo("aah"));
+  }
+  */
